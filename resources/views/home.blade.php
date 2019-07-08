42bf7fe07ae064bbc0a508eaa9604fe0c287d138
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-07-08 22:33:40 +0200

    footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,16 @@
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 @extends('layouts.base')
 
-@section('diventa_sviluppatore')
-<div class="row diventa_sviluppatore">
-  <div class="sinistro col-xs-12 col-md-6 col-lg-offset-2 col-lg-4">
-    <h1 class="text-success">Diventa sviluppatore web</h1>
-    <p>6 mesi di corso intensivo online in diretta per imparare da zero il mestiere di web developer e trovare un nuovo lavoro nelle aziende top del settore</p>
+@section('content')
+  <div class="row diventa_sviluppatore">
+    <div class="sinistro col-xs-12 col-md-6 col-lg-offset-2 col-lg-4">
+      <h1 class="text-success">Diventa sviluppatore web</h1>
+      <p>6 mesi di corso intensivo online in diretta per imparare da zero il mestiere di web developer e trovare un nuovo lavoro nelle aziende top del settore</p>
+    </div>
+    <div class="destro col-xs-12 col-md-6 col-lg-6">
+      <img class="notebook_img" src="https://www.boolean.careers/images/common/mac2x.png" alt="">
+    </div>
   </div>
-  <div class="destro col-xs-12 col-md-6 col-lg-6">
-    <img class="notebook_img" src="https://www.boolean.careers/images/common/mac2x.png" alt="">
-  </div>
-</div>
-@endsection
-
-@section('assunti_da')
   <div class="partners">
       <div class="row text-center">
         <div class="col-xs-12 col-md-12">
@@ -24,19 +21,18 @@
         <div class="col-xs-12 col-md-2 offset-md-1">
           <img src="https://www.boolean.careers/images/partners/facile_it.png" alt="">
         </div>
-        <div class="col-xs-12 col-md-2 ">
+        <div class="col-xs-12 col-md-2">
           <img src="https://www.boolean.careers/images/partners/oneday.png" alt="">
         </div>
-        <div class="col-xs-12 col-md-2 ">
+        <div class="col-xs-12 col-md-2">
           <img src="https://www.boolean.careers/images/partners/cayenne.png" alt="">
         </div>
-        <div class="col-xs-12 col-md-2 ">
+        <div class="col-xs-12 col-md-2">
           <img src="https://www.boolean.careers/images/partners/softec.png" alt="">
         </div>
-        <div class="col-xs-12 col-md-2 ">
+        <div class="col-xs-12 col-md-2">
           <img src="https://www.boolean.careers/images/partners/fabbrica_digitale.png" alt="">
         </div>
       </div>
   </div>
-
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
   </head>
   <body>
     <header>
@@ -18,10 +19,34 @@
       </nav>
 
       <div id="main_content">
-      @yield('diventa_sviluppatore')
-      @yield('assunti_da')
+      @yield('content')
       </div>
-
     </header>
+    <footer class="fixed-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-auto col-12">
+            <a href="/" title="Home Boolean"><img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean logo"> </a>
+          </div>
+          <div class="col-lg-auto col-12" id="footer_recapiti">
+            <a href="mailtoo:ciao@boolean.careers">ciao@boolean.careers</a>
+            <a href="tel:02-40031288">02-40031288</a>
+            <a href="tel:+39 327-9578239">+39 327-9578239</a>
+          </div>
+          <div class="col-lg-auto col-12" id="footer_menu">
+            <a href="#">Domande Frequenti</a>
+            <a href="#">Lezione Gratuita</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Lavora con noi!</a>
+          </div>
+          <div class="col-lg-auto col-12" id="footer_social">
+            <a href="#"><i class="fab fa-facebook-square black fa-2x"></i></a>
+            <a href="#"><i class="fab fa-linkedin black fa-2x"></i></a>
+            <a href="#"><i class="fab fa-youtube black fa-2x"></i></a>
+            <a href="#"><i class="fab fa-whatsapp black fa-2x"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
   </body>
 </html>
