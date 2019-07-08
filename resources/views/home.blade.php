4899b2f1a44dddd805a58e3231aefea8f7e3b07a
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-07-08 13:57:05 +0200

    Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+@extends('layouts.base')
+
+@section('diventa_sviluppatore')
+<div class="diventa_sviluppatore">
+  <div class="sinistro col-lg-4 col-md-8 col-12">
+    <h1 class="text-success">Diventa sviluppatore web</h1>
+    <p>6 mesi di corso intensivo online in diretta per imparare da zero il mestiere di web developer e trovare un nuovo lavoro nelle aziende top del settore</p>
+  </div>
+  <div class="destro col-lg-4 col-md-8 col-12">
+    <img class="notebook_img" src="https://www.boolean.careers/images/common/mac2x.png" alt="">
+  </div>
+</div>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  </head>
+  <body>
+    <header>
+      <img src="https://www.boolean.careers/images/common/logo.png" alt="Boolean logo">
+      <nav>
+        <a class="btn" href="#">Home</a>
+        <a class="btn" href="#">Corso</a>
+        <a class="btn" href="#">Metodo</a>
+        <a class="btn" href="#">Dopo il corso</a>
+        <a class="btn" href="#">Lezione gratuita</a>
+        <a class="btn" href="#">Iscriviti</a>
+      </nav>
+
+      <div id="main_content">
+      @yield('diventa_sviluppatore')
+      @yield('assunti_da')
+      </div>
+
+    </header>
+  </body>
+</html>
